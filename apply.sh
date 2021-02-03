7544bce830d710870bb41f8555cb67c9b9e9e059
Lexevo <31176843+Lexevolution@users.noreply.github.com>
2021-02-03 16:01:51 +1000

    Added openbox's rc.xml

--- apply.sh
+++ apply.sh
@@ -3,6 +3,7 @@ DOTLS=/home/alex/Documents/dotfiles
 cd $DOTLS
 cp ./.bashrc ~/.bashrc
 cp ./openbox/autostart ~/.config/openbox/autostart
+cp ./openbox/rc.xml ~/.config/openbox/rc.xml
 cp ./tint2/dock ~/.config/tint2/dock
 cp ./tint2/status ~/.config/tint2/status
 cp ./picom/picom.conf ~/.config/picom/picom.conf
